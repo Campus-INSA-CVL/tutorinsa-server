249ec22bf109dc9a14571684f5f79531ef179d4b
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-05-07 00:47:13 +0200

    lint all files

--- src/logger.ts
+++ src/logger.ts
@@ -6,39 +6,52 @@ const logFormat = format.printf(
   (info) => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
 )
 
-// Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
-const logger = createLogger({
+// Options for the logger
+const options: {
+  level: string
+  format: any
+  transports: (
+    | transports.ConsoleTransportInstance
+    | transports.FileTransportInstance
+  )[]
+  exitOnError: boolean
+} = {
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   format: format.combine(
     format.label({
       label: path.basename(process.mainModule!?.filename ?? 'test'),
     }),
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    // Format the metadata object
-    format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] })
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' })
   ),
-  transports: [
-    new transports.Console({
-      format: format.combine(format.colorize(), logFormat),
-    }),
-  ],
+  transports: [],
   exitOnError: false,
-})
-
-if (process.env.NODE_ENV !== 'test') {
-  logger.transports.push(
-    // Every logs
-    new transports.File({
-      filename: 'logs',
-      format: format.combine(format.json()),
-    }),
-    // Error log
-    new transports.File({
-      level: 'error',
-      filename: 'errors.log',
-      format: format.combine(format.json()),
-    })
-  )
 }
+
+// Add transports depending of the env
+if (process.env.NODE_ENV !== 'development') {
+  const consoleLogs = new transports.Console({
+    format: format.combine(format.colorize(), logFormat),
+  })
+
+  options.transports.push(consoleLogs)
+}
+
+if (process.env.NODE_ENV === 'production') {
+  const logs = new transports.File({
+    filename: 'logs',
+    format: format.combine(format.json()),
+  })
+  const errorslog = new transports.File({
+    level: 'error',
+    filename: 'errors.log',
+    format: format.combine(format.json()),
+  })
+
+  options.transports.push(logs)
+  options.transports.push(errorslog)
+}
+
+// Create logger
+const logger = createLogger(options)
 
 export default logger
