1e8322252502448b2cfd54980f1821ea4f9fbdb1
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-05-06 09:07:15 +0200

    initial commit

---
+++ src/middleware/index.ts
@@ -0,0 +1,5 @@
+import { Application } from '../declarations'
+// Don't remove this comment. It's needed to format import lines nicely.
+
+/* tslint:disable:no-empty */
+export default function (app: Application) {}
