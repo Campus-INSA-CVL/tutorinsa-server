192c7f7fd4c4466dbaa8a17a41ce4adcf79a5b55
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-05-13 19:48:41 +0200

    finish test for check date

--- test/hooks/check/check-room/check-date.test.ts
+++ test/hooks/check/check-room/check-date.test.ts
@@ -1,6 +1,7 @@
 import checkDate from '../../../../src/hooks/check/check-room/check-date'
 import { HookContext, Application, Service } from '@feathersjs/feathers'
 import { Room } from '../../../../src/declarations'
+import { BadRequest } from '@feathersjs/errors'
 
 describe("'check-date' hook", () => {
   let context: HookContext<Room>
@@ -20,7 +21,51 @@ describe("'check-date' hook", () => {
     error = null
   })
 
-  it.todo('nothing should append with correct data')
+  it('nothing should append with correct data', async () => {
+    expect.assertions(2)
+    const data = {
+      startAt: 'Wed May 13 2020 19:30:00 GMT+0000',
+      duration: 120,
+    }
 
-  it.todo('should throw an error because of incorrect field %s')
+    context.data = data as Room
+
+    try {
+      result = (await checkDate()(context)) as HookContext<Room>
+    } catch (e) {
+      error = e
+    }
+
+    expect(error).toBeNull()
+    expect(result).toEqual(context)
+  })
+
+  it.each([
+    ['startAt', 'Wed May 13 2020 19:36:00 GMT+0000'],
+    ['duration', 42],
+  ])(
+    'should throw an error because of incorrect field %s',
+    async (key, value) => {
+      expect.assertions(2)
+
+      const data: object = {}
+      data[key] = value
+
+      context.data = data as Room
+
+      try {
+        await checkDate()(context)
+      } catch (e) {
+        error = e
+      }
+
+      if (key === 'startAt') {
+        expect(error).toBeInstanceOf(BadRequest)
+        expect(error.message).toBe('minutes must be 00 or 30')
+      } else if (key === 'duration') {
+        expect(error).toBeInstanceOf(BadRequest)
+        expect(error.message).toBe('duration must be a multiple of 30')
+      }
+    }
+  )
 })
