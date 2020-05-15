9af59efba6adccfd45bfbecd206b5f8ded75faef
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-05-15 21:14:36 +0200

    create and test removeUnwantedFields hook

--- src/hooks/remove-unwanted-fields.ts
+++ src/hooks/remove-unwanted-fields.ts
@@ -1,9 +1,33 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers'
+import { Post } from '../declarations'
 
-export default (options = {}): Hook => {
-  return async (context: HookContext) => {
+/**
+ * Remove some fields in data
+ * @param fields
+ * @param data
+ * @return Post
+ */
+function removeFields(fields: string[], data: Post): Post {
+  fields.forEach((field) => {
+    delete data[field]
+  })
+  return data
+}
+
+/**
+ * Remove unwanted fields in data passed through options
+ * @param options
+ */
+export default (options?: string[]): Hook => {
+  return async (context: HookContext<Post>) => {
+    const { data } = context
+
+    if (data && options?.length) {
+      context.data = removeFields(options, data)
+    }
+
     return context
   }
 }
--- test/hooks/remove-unwanted-fields.test.ts
+++ test/hooks/remove-unwanted-fields.test.ts
@@ -1,1 +1,113 @@
-// describe("'remove-unwanted-fields' hook", () => {})
+import removeUnwantedFields from '../../src/hooks/remove-unwanted-fields'
+import { Post } from '../../src/declarations'
+import { HookContext, Application, Service } from '@feathersjs/feathers'
+
+describe("'remove-unwanted-fields' hook", () => {
+  let context: HookContext<Post>
+
+  let error: Error | null
+  let result: HookContext<Post>
+
+  beforeEach(() => {
+    context = {
+      app: {} as Application,
+      service: {} as Service<Post>,
+      method: 'create',
+      params: {},
+      path: '',
+      type: 'before',
+    }
+    result = null
+    error = null
+  })
+  it('nothing should append without data and options', async () => {
+    expect.assertions(2)
+
+    try {
+      result = (await removeUnwantedFields()(context)) as HookContext<Post>
+    } catch (e) {
+      error = e
+    }
+
+    expect(error).toBeNull()
+    expect(result).toEqual(context)
+  })
+
+  it('nothing should append without options', async () => {
+    expect.assertions(2)
+
+    const data = { comment: 'Hello everyone !' }
+
+    context.data = data as Post
+
+    try {
+      result = (await removeUnwantedFields()(context)) as HookContext<Post>
+    } catch (e) {
+      error = e
+    }
+
+    expect(error).toBeNull()
+    expect(result).toEqual(context)
+  })
+  it('nothing should append without data', async () => {
+    expect.assertions(2)
+
+    try {
+      result = (await removeUnwantedFields(['data'])(context)) as HookContext<
+        Post
+      >
+    } catch (e) {
+      error = e
+    }
+
+    expect(error).toBeNull()
+    expect(result).toEqual(context)
+  })
+
+  it.each(['creatorId', 'studentsIds'])(
+    'field %s should have been deleted',
+    async (key) => {
+      expect.assertions(2)
+      const data: object = {}
+      data[key] = 'data'
+
+      context.data = data as Post
+
+      try {
+        result = (await removeUnwantedFields([key])(context)) as HookContext<
+          Post
+        >
+      } catch (e) {
+        error = e
+      }
+
+      expect(error).toBeNull()
+      expect(result.data[key]).toBeUndefined()
+    }
+  )
+
+  it('should delete many fields in one time', async () => {
+    expect.assertions(4)
+
+    const data = {
+      comment: 'some data',
+      studentsIds: ['data ?'],
+      tutorsIds: ['yes, data !'],
+    }
+
+    context.data = data as Post
+
+    try {
+      result = (await removeUnwantedFields(['studentsIds', 'tutorsIds'])(
+        context
+      )) as HookContext<Post>
+    } catch (e) {
+      error = e
+    }
+
+    expect(error).toBeNull()
+    expect(result.data.comment).toBe(data.comment)
+    expect(result.data.studentsIds).toBeUndefined()
+    expect(result.data.tutorsIds).toBeUndefined()
+  })
+})
