868cfd325d405f3606ed28333b9c5c54d7c134a9
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-06-15 05:10:42 +0200

    create a script for deployment

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -3,8 +3,6 @@ import { Application } from './declarations'
 import logger from './logger'
 
 export default function (app: Application) {
-  /*tslint:disable*/
-  console.log(app.get('mongodb'))
   mongoose
     .connect(app.get('mongodb'), {
       useCreateIndex: true,
