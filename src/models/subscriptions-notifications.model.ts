3630103099c456a94f5c91384f78ae43dcd53116
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-31 23:07:41 +0200

    create schema for subscriptions

--- src/models/subscriptions-notifications.model.ts
+++ src/models/subscriptions-notifications.model.ts
@@ -3,20 +3,39 @@
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 import { Application } from '../declarations'
-import { Model, Mongoose } from 'mongoose'
+import mongoose, { Model, Mongoose } from 'mongoose'
+
+const objSchema = {
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  endpoint: {
+    type: String,
+    required: true,
+  },
+  keys: {
+    p256dh: {
+      type: String,
+      required: true,
+    },
+    auth: {
+      type: String,
+      required: true,
+    },
+  },
+}
+
+const objOptions = {
+  timestamps: true,
+}
 
 export default function (app: Application): Model<any> {
   const modelName = 'subscriptionsNotifications'
   const mongooseClient: Mongoose = app.get('mongooseClient')
   const { Schema } = mongooseClient
-  const schema = new Schema(
-    {
-      text: { type: String, required: true },
-    },
-    {
-      timestamps: true,
-    }
-  )
+  const schema = new Schema(objSchema, objOptions)
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
