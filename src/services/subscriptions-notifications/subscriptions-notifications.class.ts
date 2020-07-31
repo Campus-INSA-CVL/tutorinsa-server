f3db266f52578b56961f7299141921f4775a197a
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-31 22:35:20 +0200

    add user to subscription

--- src/services/subscriptions-notifications/subscriptions-notifications.class.ts
+++ src/services/subscriptions-notifications/subscriptions-notifications.class.ts
@@ -1,9 +1,21 @@
+import { GeneralError } from '@feathersjs/errors'
+import { Id, Params } from '@feathersjs/feathers'
 import { Service, MongooseServiceOptions } from 'feathers-mongoose'
-import { Application } from '../../declarations'
+import { Application, User } from '../../declarations'
 
 export class SubscriptionsNotifications extends Service {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<MongooseServiceOptions>, app: Application) {
     super(options)
+  }
+  async create(data: PushSubscription & { userId: Id }, params: Params) {
+    const { user } = params
+    if (!(user as User)._id) {
+      throw new GeneralError(
+        'a user must be provided to subscribe to notification'
+      )
+    }
+    data.userId = (user as User)._id?.toString()!
+    return super.create(data, params)
   }
 }
